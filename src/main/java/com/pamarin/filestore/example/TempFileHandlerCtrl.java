205f13bb0ea5a595e4b412b8006902ca7066f34f
jittagornp <jittagornp@gmail.com>
2018-03-30 13:40:47 +0700

    add share link

--- src/main/java/com/pamarin/filestore/example/TempFileHandlerCtrl.java
+++ src/main/java/com/pamarin/filestore/example/TempFileHandlerCtrl.java
@@ -3,8 +3,12 @@
  */
 package com.pamarin.filestore.example;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.pamarin.filestore.FileHandlerAdapter;
 import com.pamarin.filestore.FileUploader;
+import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(TempAccessPathFileRequestConverter.CONTEXT_PATH)
 public class TempFileHandlerCtrl extends FileHandlerAdapter {
 
+    private static final String SHARE_SECRET_KEY = "secret";
+
     @Autowired
     private TempFileUploader fileUploader;
 
@@ -28,6 +34,31 @@ public class TempFileHandlerCtrl extends FileHandlerAdapter {
     @Override
     protected String getUserId(HttpServletRequest httpReq) {
         return "1";//TODO : may be get value from SecurityContext
+    }
+
+    @Override
+    public String getShareToken(String userId) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC384(SHARE_SECRET_KEY);
+            return JWT.create()
+                    .withIssuer(userId)
+                    .sign(algorithm);
+        } catch (UnsupportedEncodingException | JWTCreationException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public String verifyShareToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC384(SHARE_SECRET_KEY);
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getIssuer();
+        } catch (UnsupportedEncodingException | JWTCreationException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
